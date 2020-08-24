fd17034f11acd51456827785be8e0556872aff78
TheMaoci <themaoci@gmail.com>
2020-08-24 14:33:05 +0200

    v0.7.2
    useless changes between 0.7.0 and this one...

---
+++ Server/src/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const logger = require('./logger.js');
+const server = require('./server.js');
+const trader = require('./trader.js');
+function setTitle(title) {
+    process.stdout.write(
+        String.fromCharCode(27) + ']0;' + title + String.fromCharCode(7)
+    );
+}
+
+setTitle("JustEmuTarkov Server " + server.version());
+logger.start();
+server.start();
+trader.load();
